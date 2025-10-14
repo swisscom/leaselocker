9b9256fc389e660b42deffa588d84e07f4f6d6b3
Joel Studler <joel.studler@swisscom.com>
2025-10-14 08:16:49 +0200

    Clean up import names

--- leaselock_test_helpers.go
+++ leaselock_test_helpers.go
@@ -20,13 +20,13 @@ import (
 	"context"
 	"fmt"
 
-	v2 "k8s.io/api/coordination/v1"
+	coordv1 "k8s.io/api/coordination/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/watch"
-	v4 "k8s.io/client-go/applyconfigurations/coordination/v1"
-	v3 "k8s.io/client-go/kubernetes/typed/coordination/v1"
+	applyconfigcoordv1 "k8s.io/client-go/applyconfigurations/coordination/v1"
+	kubernetestypedcoordv1 "k8s.io/client-go/kubernetes/typed/coordination/v1"
 	"k8s.io/client-go/rest"
 )
 
@@ -36,7 +36,7 @@ func (f *fakeLeaseClient) DeleteCollection(ctx context.Context, opts v1.DeleteOp
 
 // Mocks both LeasesGetter and LeaseInterface
 type fakeLeaseClient struct {
-	lease     *v2.Lease
+	lease     *coordv1.Lease
 	createErr error
 	getErr    error
 	updateErr error
@@ -71,25 +71,25 @@ func (f *fakeLeaseClient) RESTClient() rest.Interface {
 }
 
 // Implements coordinationv1.LeasesGetter
-func (f *fakeLeaseClient) Leases(ns string) v3.LeaseInterface {
+func (f *fakeLeaseClient) Leases(ns string) kubernetestypedcoordv1.LeaseInterface {
 	return f
 }
 
 // Implements coordinationv1.LeaseInterface
-func (f *fakeLeaseClient) Get(ctx context.Context, name string, opts v1.GetOptions) (*v2.Lease, error) {
+func (f *fakeLeaseClient) Get(ctx context.Context, name string, opts v1.GetOptions) (*coordv1.Lease, error) {
 	if f.getErr != nil {
 		return nil, f.getErr
 	}
 	return f.lease, nil
 }
-func (f *fakeLeaseClient) Create(ctx context.Context, lease *v2.Lease, opts v1.CreateOptions) (*v2.Lease, error) {
+func (f *fakeLeaseClient) Create(ctx context.Context, lease *coordv1.Lease, opts v1.CreateOptions) (*coordv1.Lease, error) {
 	if f.createErr != nil {
 		return nil, f.createErr
 	}
 	f.lease = lease
 	return lease, nil
 }
-func (f *fakeLeaseClient) Update(ctx context.Context, lease *v2.Lease, opts v1.UpdateOptions) (*v2.Lease, error) {
+func (f *fakeLeaseClient) Update(ctx context.Context, lease *coordv1.Lease, opts v1.UpdateOptions) (*coordv1.Lease, error) {
 	if f.updateErr != nil {
 		return nil, f.updateErr
 	}
@@ -101,16 +101,16 @@ func (f *fakeLeaseClient) Update(ctx context.Context, lease *v2.Lease, opts v1.U
 func (f *fakeLeaseClient) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
 	return nil
 }
-func (f *fakeLeaseClient) List(ctx context.Context, opts v1.ListOptions) (*v2.LeaseList, error) {
+func (f *fakeLeaseClient) List(ctx context.Context, opts v1.ListOptions) (*coordv1.LeaseList, error) {
 	return nil, nil
 }
 func (f *fakeLeaseClient) Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error) {
 	return nil, nil
 }
-func (f *fakeLeaseClient) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (*v2.Lease, error) {
+func (f *fakeLeaseClient) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (*coordv1.Lease, error) {
 	return nil, nil
 }
-func (f *fakeLeaseClient) Apply(ctx context.Context, lease *v4.LeaseApplyConfiguration, opts v1.ApplyOptions) (*v2.Lease, error) {
+func (f *fakeLeaseClient) Apply(ctx context.Context, lease *applyconfigcoordv1.LeaseApplyConfiguration, opts v1.ApplyOptions) (*coordv1.Lease, error) {
 	return nil, nil
 }
 
